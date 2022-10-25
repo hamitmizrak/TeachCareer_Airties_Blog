fdda0bc396e0491b06b8cd9ef72108209e2c03e4
hamitmizrak <hamitmizrak@gmail.com>
2022-10-25 19:08:39 +0300

    regsiter.html , RegisterDto, RegisterController

--- src/main/java/com/hamitmizrak/business/dto/RegisterDto.java
+++ src/main/java/com/hamitmizrak/business/dto/RegisterDto.java
@@ -1,10 +1,17 @@
 package com.hamitmizrak.business.dto;
 
 
+import com.hamitmizrak.annotation.UserRegisterUniqueEmail;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
 import java.util.Date;
 
 //lombok
@@ -12,12 +19,24 @@ import java.util.Date;
 @NoArgsConstructor
 @Data
 @Builder
-public class RegisterDto {
+public class RegisterDto implements Serializable {
 
     private Long id;
+    @NotNull(message = "{blog.username.validation.constraints.NotNull.message}")
     private String name;
+
+    @NotNull(message = "{blog.surname.validation.constraints.NotNull.message}")
     private String surname;
+
+    @NotNull(message = "{blog.email.validation.constraints.NotNull.message}")
+    @Email(message = "{blog.unique.email.validation.constraints.NotNull.message}")
+    @UserRegisterUniqueEmail
     private String email;
+
+    @NotNull(message = "{blog.password.validation.constraints.NotNull.message}")
+    @Size(min=7,max = 12,message = "{blog.password.pattern.validation.constraints.NotNull.message}")
+    //@Pattern(regexp = "")
     private String password;
+
     private Date createdDate;
 }
---
+++ src/main/java/com/hamitmizrak/ui/mvc/RegisterController.java
@@ -0,0 +1,42 @@
+package com.hamitmizrak.ui.mvc;
+
+import com.hamitmizrak.business.dto.RegisterDto;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@Log4j2
+public class RegisterController {
+
+    // http://localhost:8080/validation/register
+    @GetMapping("/validation/register")
+    public String validationGetRegister(Model model){
+        model.addAttribute("key_register",new RegisterDto());
+        return "register";
+    }
+
+    // http://localhost:8080/validation/register
+    @PostMapping("/validation/register")
+    public String validationPostRegister(@Valid @ModelAttribute("key_register") RegisterDto registerDto,
+            BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            log.error("HATA: "+bindingResult.hasErrors());
+            return "register";
+        }
+        //eğer valiadtion bir hata yoksa
+        model.addAttribute("register_success","Üye Kaydı Başarılı "+registerDto);
+        log.info("Başarılı "+registerDto);
+
+        //Database
+        //File
+        return "success";
+    }
+
+}
